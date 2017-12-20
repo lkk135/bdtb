d9ddf40346628c6512fadb50a2f6270b4ea23cda
zwj4386 <31980852+zwj4386@users.noreply.github.com>
2017-12-20 15:15:43 +0800

    Update pipelines.py

--- pipelines.py
+++ pipelines.py
@@ -12,7 +12,7 @@ import pymysql
 os.environ['NLS_LANG']='SIMPLIFIED CHINESE_CHINA.UTF-8'
 class BdtbPipeline(object):
     def process_item(self, item, spider):
-        conn=pymysql.connect(host='192.168.3.232',user='zwj',passwd='123456',db='caiji',charset='utf8',port=3306)
+        conn=pymysql.connect(host='ip',user='zwj',passwd='123456',db='test',charset='utf8',port=3306)
         cursor = conn.cursor()
         sql_insert = "insert into TOP_BDTBRESULT(title,content,site,author,fbsj,cjmc,url) values('%s','%s','%s','%s','%s','%s','%s')" % (item['title'], item['content'], item['site'],item['author'], item['fbsj'],item['cjmc'],item['url'])
         cursor.execute(sql_insert)
